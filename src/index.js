8f2f50b0c3ae56d8f8a5ffcc9c7876275ce847da
Dmitry Shelomanov <userbq201@mail.ru>
2019-09-28 22:34:10 +0300

    feat: add quest states

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
 // @flow strict
 
 export * from "./equipment"
+export * from "./quests"
---
+++ src/quests.js
@@ -0,0 +1,8 @@
+//  @flow strict
+
+export const QuestStates = {
+  active: 1,
+  available: 2,
+  completed: 3,
+  notAvailable: 4,
+}
